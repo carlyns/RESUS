3425c26111c49e5eb43b31df55a93970ad046529
carlyn <carlyns@gmail.com>
2017-01-31 16:10:50 -0600

    pulled down current site
    with fewd styling

---
+++ fireplace/ published sep2016/js/fireplace.js
@@ -0,0 +1,81 @@
+// collapse all sections on page load
+var carlyn = 2;
+
+// show a reminder "Start your profile now, and we’ll remind you to complete it once we officially launch. " on hover of first Start Profile anchor tag in Overview.
+
+
+
+
+
+// show "it's free" to appear on hover of button when submitting the form
+
+
+
+
+// on click of Submit button,
+$('.submitbttn').on("click",function(){
+  // save all inputs in variables
+  firstN = $('#firstname').val();
+  lastN = $('#lastname').val();
+  lastinitial = lastN.charAt(0); // DOES VAR NEED TO BE WRAPPED IN () ??
+  email = $('#email').val();
+  password = $('#password').val();
+  housingtype = $('#housingtype').val();
+  if ($('#savescreds').is(":checked")) {
+    savescreds = true;
+  }
+  else {
+    savescreds = false;
+  }
+
+
+  // hide form
+  $('#profileform form').hide(function() {
+      $(this).animate({height: 'auto'});
+  }, function() {
+      $(this).animate({height: 'auto'});
+  });
+
+
+  // show popout button?
+  function displayThanks() {
+    $('#profileform h2').html(
+      'you\'ve started a profile'
+    );
+    $('#profileform h3').html(
+      '<p>' + 'Thanks, ' + firstN + '!  </p>'
+      + '<p>' + lastinitial + '</p>'
+      + '<p>Ooh, ' + housingtype + '... nice choice.  We\'re excited for you to get started. <br> We\'ll notify you when the tool is up and running. </p>'
+    );
+  }
+  displayThanks();
+});
+
+
+
+// stop refresh on submit click
+$('form').submit(function(e) {
+  e.preventDefault();
+});
+
+
+
+
+
+
+  // smooth scrolling to anchors on page
+  // snippet from https://css-tricks.com/snippets/jquery/smooth-scrolling/
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
