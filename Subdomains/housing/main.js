0dd2cf0251f7683317b7c5f76830f15e4332c3d9
carlyn <carlyns@gmail.com>
2018-03-28 19:46:51 -0500

    housing landing edits
    fixing images, including favicon, trying to fix JS issue but reverted
    to bootstrap JS,

--- Subdomains/housing/main.js
+++ Subdomains/housing/main.js
@@ -1,5 +1,23 @@
 var main = function() {
 
+  // (bootstratp jquery worked but then custom jquery did not fire.  but adding jquery manually caused bootstrap jquery to stop firing. so adding orig bootstratp jquery in this custom file. )
+
+  // TOGGLING TEXT
+  $('#optionA').click(function()) {
+    $('#structureA')
+  };
+  $('#optionB').click(function()) {
+    $('#structureB')
+  };
+  $('#optionC').click(function()) {
+    $('#structureC')
+  };
+
+  $('#morelimeq').click(function()) {
+    $('#limeq')
+  };
+
+
   // SMOOTH SCROLLING TO ANCHORS ON PAGE, from CSS Tricks
   $(function() {
     $('a[href*="#"]:not([href="#"])').click(function() {
